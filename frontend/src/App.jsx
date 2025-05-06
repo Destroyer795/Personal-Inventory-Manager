f706aea687e684bc52e9a02ca66ae6dc7079751d
pc <ty.pranavkishan7905@gmail.com>
2025-05-06 10:12:43 +0530

    Rectified MongoDB errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,20 @@
+//App.jsx
 import { Box, useColorModeValue } from '@chakra-ui/react';
 import { Route, Routes } from 'react-router-dom';
 import CreatePage from './pages/CreatePage';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
 
-import {
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn,
-  SignIn,
-  SignUp,
-} from '@clerk/clerk-react';
-
 function App() {
   return (
-    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
-      {/*Show only if the user is signed in */}
-      <SignedIn>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create" element={<CreatePage />} />
-        </Routes>
-      </SignedIn>
-
-      {/*redirect if not signed in */}
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-
-      {/*Clerk Auth Routes */}
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar/>
       <Routes>
-        <Route path="/sign-in/*" element={<SignIn routing="path" path="/sign-in" />} />
-        <Route path="/sign-up/*" element={<SignUp routing="path" path="/sign-up" />} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/create" element={<CreatePage/>} />
       </Routes>
     </Box>
-  );
+  )
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,16 @@
+//main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
-
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <BrowserRouter>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
